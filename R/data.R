33fb667240e6eea490ca7cf6491dffeab715328f
Sigve Nakken <sigven@ifi.uio.no>
2020-07-09 14:13:58 +0200

    0.6.5 release

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Drug-target network in cancer
+#'
+#' A dataset containing connections between drugs (ChEMBL compounds) and proteins (entrez gene
+#' identifiers).
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
